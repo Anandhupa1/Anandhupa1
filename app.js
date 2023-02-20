23e201b1ac44cf245341634a224c2182835f4f35
Anandhu_P_A <anandhupa131@gmail.com>
2023-02-20 14:03:05 +0530

    portfolio todays work

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ hamburger.addEventListener('click', () => {
 document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
 	if (scroll_position > 250) {
-		header.style.backgroundColor = '#fff';
+		header.style.backgroundColor = '#E6E6E6';
 	} else {
 		header.style.backgroundColor = 'transparent';
 	}
